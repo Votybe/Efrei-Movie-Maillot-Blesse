09b947bdb5328745104b699299502996b8533df0
Votybe <aymmai@gmail.com>
2021-10-15 23:49:49 +0200

    style films

--- src/views/GetAllFIlms.vue
+++ src/views/GetAllFIlms.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="section"></div>
+  <div class="section">
+    <ul>
+      <li v-for="(film, index) in films" :key="index">
+        <div class="container-film">
+          <img
+            :src="
+              'https://www.themoviedb.org/t/p/w220_and_h330_face/' +
+                film.backdrop_path
+            "
+            :alt="film.original_title"
+          />
+          <p class="film-title">
+            {{ film.original_title }}
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -32,62 +49,50 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Ephesis&display=swap");
+
 .section {
   height: 100%;
   display: flex;
   background-color: #3894b2;
   flex-direction: column;
-
   ul {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
-
     li {
       list-style: none;
     }
     .container-film {
+      position: relative;
       margin: 2rem;
       background-color: green;
       flex-direction: column;
-      width: 200px;
-      height: 200px;
+      width: 350px;
+      height: 470px;
       display: flex;
       align-items: center;
-      padding: 45px;
-      border-radius: 25px;
       background: linear-gradient(72deg, #343a40 0%, #0d0d0d 80%);
       background: -webkit-linear-gradient(72deg, #343a40 0%, #0d0d0d 80%);
       background: -moz-linear-gradient(72deg, #343a40 0%, #0d0d0d 80%);
       .film-title {
+        background-color: #0d0d0d;
+        padding: 20px;
+        position: absolute;
+        bottom: 0;
         width: 100%;
         text-align: center;
-        font-size: 1.5rem;
+        font-size: 2rem;
+        max-width: 80%;
         font-weight: 600;
         color: white;
-        &:hover {
-          text-decoration: underline white 5px;
-        }
-      }
-
-      .film-average {
-        margin: 1rem;
-        font-size: 1.4rem;
-      }
-
-      .film-date {
-        margin: 2rem;
-        font-size: 1rem;
-      }
-      p {
-        width: 45%;
+        font-family: Ephesis;
       }
       img {
-        width: 50px;
-        height: 50px;
+        width: 100%;
+        height: 100%;
         border-radius: 20px;
-        margin: 1rem;
       }
     }
   }
